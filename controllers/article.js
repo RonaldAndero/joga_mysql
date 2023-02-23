e7931bcecfb772af11fad23cacb772742b67255a
Ronald Andero Õisman <ronald-andero.oisman@khk.ee>
2023-02-24 00:55:50 +0200

    Created author controller and route, refactored application code

--- controllers/article.js
+++ controllers/article.js
@@ -14,19 +14,34 @@ const getAllArticles = (req, res) => {
 };
 //show article by this slug
 const getArticleBySlug = (req, res) => {
-    let query = `SELECT article.*, author.name AS authorName FROM article INNER JOIN author ON article.author_id = author.id WHERE slug="${req.params.slug}"`
+    let query = `SELECT article.id, article.name, article.slug, article.image, article.body, article.published, author.name AS author, author.id AS author_id FROM article JOIN author ON article.author_id = author.id WHERE slug = "${req.params.slug}";`
     let article
     con.query(query, (err, result) => {
-        if (err) throw err;
-        console.log(result)
-        article = result;
+        if (err) throw err
+        article = result
         res.render('article', {
             article: article
         })
-    });
-};
+    })
+}
+
+const getArticlesByAuthor = (req, res) => {
+    let query = `SELECT article.id, article.name, article.slug, article.image, article.body, article.published, author.name AS author, author.id AS author_id FROM article JOIN author ON article.author_id = author.id WHERE author_id = "${req.params.author_id}";`
+    let articles = []
+    let author
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        author = result[0]
+        res.render('author', {
+            articles: articles,
+            author: author
+        })
+    })
+}
 
 module.exports = {
     getAllArticles,
-    getArticleBySlug
+    getArticleBySlug,
+    getArticlesByAuthor
 }
--- index.js
+++ index.js
@@ -1,46 +1,32 @@
-// app packages
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const path = require('path')
-// add template engine
+const path = require('path');
+
+// template engine
 const hbs = require('express-handlebars');
-
-const mysql = require('mysql')
-// setup template engine directory and files extensions
+// setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.engine('hbs', hbs.engine({
     extname: 'hbs',
     defaultLayout: 'main',
-    layoutsDir: __dirname + '/views/layouts',
-}))
-// setup static public directory
+    layoutsDir: __dirname + '/views/layouts/',
+}));
+
+// public directory
 app.use(express.static('public'));
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({extended: true}))
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const articleRoutes = require('./routes/article');
 
-app.use('/', articleRoutes)
-app.use('/article', articleRoutes)
+app.use('/', articleRoutes);
+app.use('/article', articleRoutes);
+app.use('/author', articleRoutes);
 
-
-// show articles by author
-app.get('/author/:id', (req, res) => {
-    let query = `SELECT * FROM article WHERE author_id ="${req.params.author_id}"`
-    let articles
-    con.query(query, (err, result) => {
-        if (err) throw err;
-        author = result
-        res.render('author', {
-            author: author,
-            articles: articles
-        })
-    })
-});
-// app start point
+// start app
 app.listen(3000, () => {
     console.log("App is started at http://localhost:3000");
 });
